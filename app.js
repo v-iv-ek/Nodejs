a98229ff84cb2b88e7d78f4fa642c87bbc860de9
v-iv-ek <tadakamadlavivek@gmail.com>
2024-05-18 19:07:40 +0530

    Added /home /about /node routes in server

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
-const http=require('http')
-const server=http.createServer(function(req,res){
-    console.log("vivek");
-    
-})
+const http = require("http");
+const server = http.createServer(function (req, res) {
+  if (req.url == "/home") {
+    res.write("<html>");
+    res.write("<head><title>Req and Res</title></head>");
+    res.write("<body><h1>Welcome home</h1></body>");
+    res.write("</html>");
+    res.end();
+  }
+  if (req.url == "/about") {
+    res.write("<html>");
+    res.write("<head><title>Req and Res</title></head>");
+    res.write("<body><h1>Welcome to  About Us page  </h1></body>");
+    res.write("</html>");
+    res.end();
+  }
+  if (req.url == "/node") {
+    res.write("<html>");
+    res.write("<head><title>Req and Res</title></head>");
+    res.write("<body><h1>Welcome to my NodeJS Project</h1></body>");
+    res.write("</html>");
+    res.end();
+  }
+  // process.exit()
+});
 server.listen(4000);
-
-
