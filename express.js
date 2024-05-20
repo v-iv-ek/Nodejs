bcb84404554e8f632001d870ea988bd278f44109
v-iv-ek <tadakamadlavivek@gmail.com>
2024-05-21 02:33:51 +0530

    added routes seperate folder

--- express.js
+++ express.js
@@ -1,17 +1,14 @@
 const express=require('express');
 const bodyParser=require('body-parser')
 const app=express();
+const adminRoute=require('./Routes/admin')
+const shopRoute=require('./Routes/shop')
 app.use(bodyParser.urlencoded({extended:false}))
+app.use("/admin",adminRoute)
+app.use("/shop",shopRoute)
 
-app.use("/add-product",(req,res,next)=>{
-   res.send(`<form action="/product" method="POST"><input type="text" name="userName"><br><input type="tel" name="age"><button type="submit">Add Name</button></form>`)
 
-})
-app.use("/product",(req,res,next)=>{
-    console.log(req.body)
-    res.redirect("/")
-})
-app.use("/",(req,res,next)=>{
-    res.send('<h1>Home page</h1>')
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page Not Found</h1>')
 })
 app.listen(3000);
