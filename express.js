a6cda263707d98066704526c7f9d158a2e1b4320
v-iv-ek <tadakamadlavivek@gmail.com>
2024-05-21 01:09:52 +0530

    added form with two input and printed output in console by using body-parser

--- express.js
+++ express.js
@@ -1,12 +1,17 @@
 const express=require('express');
+const bodyParser=require('body-parser')
 const app=express();
+app.use(bodyParser.urlencoded({extended:false}))
 
-app.use((req,res,next)=>{
-    console.log('This is the first middleware')
-    next()
+app.use("/add-product",(req,res,next)=>{
+   res.send(`<form action="/product" method="POST"><input type="text" name="userName"><br><input type="tel" name="age"><button type="submit">Add Name</button></form>`)
+
 })
-app.use((req,res,next)=>{
-    console.log('This is the second middleware')
-    res.send({"key1":"value"})
+app.use("/product",(req,res,next)=>{
+    console.log(req.body)
+    res.redirect("/")
+})
+app.use("/",(req,res,next)=>{
+    res.send('<h1>Home page</h1>')
 })
 app.listen(3000);
